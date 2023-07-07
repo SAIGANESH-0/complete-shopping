ece23a16e7229e31e5fbac2ef2db2ba0dbd967b6
Saiganesh Voona <saiganeshvoona0@gmail.com>
2023-07-07 14:45:47 +0530

    h

--- api/index.js
+++ api/index.js
@@ -23,9 +23,9 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/category", categoryRoutes);
-app.use("/api/v1/product", productRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/product", productRoutes);
 
 //rest api
 app.get("/", (req, res) => {
